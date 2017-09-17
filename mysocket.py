21b65eaf0ea367728d435a1cbd43a67d035fbd86
James Parks <arcsecond@gmail.com>
2017-09-16 21:21:35 -0700

    seperated sock/clientsock

--- mysocket.py
+++ mysocket.py
@@ -33,14 +33,14 @@ class mysocket:
         self.sock.connect((self.host, self.port))
         self._log(1, 'making connection to: {}\nport: {}'.format(self.host, self.port))
 
-    def sendMessage(self, msg=None):
+    def sendMessage(self, sock=None, msg=None):
         try:
             msg = struct.pack('>i', len(msg)) + msg
             self._log(6, 'packed: {}'.format(msg))
-            self.sock.sendall(msg)
+            sock.sendall(msg)
 
             # look for ack
-            isAck = self.recv_msg()
+            isAck = self.recv_msg(sock)
             if isAck == 'ack':
                 self._log(3, 'message receipt acknowledged')
             else:
@@ -49,18 +49,18 @@ class mysocket:
             self._log(1, 'closing socket')
             self.sock.close()
 
-    def recv_msg(self):
-        raw_msglen = self.recvall(4)
+    def recv_msg(self, sock):
+        raw_msglen = self.recvall(sock, 4)
         if not raw_msglen:
             return None
         msglen = struct.unpack('>i', raw_msglen)[0]
-        return self.recvall(msglen)
+        return self.recvall(sock, msglen)
 
-    def recvall(self, n):
+    def recvall(self, sock, n):
         data = ''
         chunks = []
         while len(data) < n:
-            chunk = self.sock.recv(n - len(data))
+            chunk = sock.recv(n - len(data))
             chunks.append(chunk)
             if not chunk:
                 return None
@@ -93,21 +93,21 @@ class mysocket:
                 chunks = []
                 self._log(1, 'connection from: {}'.format(address))
                 # while True:
-                msg = self.recv_msg()
+                msg = self.recv_msg(clientsocket)
                 self._log(1, 'msg rcvd: {}'.format(msg))
                 clientsocket.sendall(msg)
-                self.sendMessage(msg='ack')
+                self.sendMessage(sock=clientsocket, msg='ack')
                 self._log(1, 'sending ack to client')
             finally:
                 clientsocket.close()
 
 if __name__ == '__main__':
     options, args = parse_args(sys.argv[1:])
-    host = 'gully'
+    host = '192.168.0.2'
     port = 50007
-    mys = mysocket()
+    mys = mysocket(host=host, port=port)
     if options.server:
-        mys.serve(host=host, port=port)
+        mys.serve()
 
 
 
